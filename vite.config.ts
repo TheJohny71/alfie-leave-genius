888838f34673fcdfdd6a38483745bc053ab81902
TheJohny71 <jduval71@me.com>
2024-11-26 13:55:57 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/alfie-leave-genius/", // Set the base path for GitHub Pages
   server: {
     host: "::",
     port: 8080,
